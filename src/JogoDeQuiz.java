c4f3caa204d0966a3efdbdd238dbb04a59445c03
EMane <e.manerich@hotmail.com>
2024-03-05 11:12:30 -0300

    version 1.1 Quiz

--- src/JogoDeQuiz.java
+++ src/JogoDeQuiz.java
@@ -9,6 +9,14 @@ public class JogoDeQuiz {
             "Qual é o maior planeta do nosso sistema solar?\n(A) Júpiter\n(B) Terra\n(C) Marte\n"
     };
     private static final String[] RESPOSTAS_ME = {"C", "B", "C", "A"};
+
+    private static final String[] PERGUNTAS_VF = {
+            "Brasília é a capital do Brasil. (Verdadeiro ou Falso)",
+            "O Sol é uma estrela. (Verdadeiro ou Falso)",
+            "O alfabeto tem 27 letras. (Verdadeiro ou Falso)",
+            "A Terra é plana. (Verdadeiro ou Falso)"
+    };
+    private static final boolean[] RESPOSTAS_VF = {true, true, false, false};
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rd = new Random();
@@ -17,7 +25,7 @@ public class JogoDeQuiz {
         System.out.println("\nBem-Vindo ao Jogo de Quiz!");
         System.out.println("Responda as perguntas corretamente para ganhar pontos");
 
-        boolean[] pergf = new boolean[PERGUNTAS_ME.length];
+        boolean[] pergf = new boolean[PERGUNTAS_ME.length + PERGUNTAS_VF.length];
 
         for (int i = 0; i < PERGUNTAS_ME.length; i++) {
             int indp;
@@ -36,6 +44,29 @@ public class JogoDeQuiz {
                 pont += 10;
             } else {
                 System.out.println("Resposta incorreta. A resposta certa é: " + RESPOSTAS_ME[indp]);
+            }
+        }
+
+        for (int i = 0; i < PERGUNTAS_VF.length; i++) {
+            int indp;
+            do {
+                indp = rd.nextInt(PERGUNTAS_VF.length);
+            } while (pergf[PERGUNTAS_ME.length + indp]);
+
+            pergf[PERGUNTAS_ME.length + indp] = true;
+
+            System.out.println("\nPergunta " + (i + 1 + PERGUNTAS_ME.length) + ":");
+            System.out.println(PERGUNTAS_VF[indp]);
+            System.out.println("Sua resposta (Verdadeiro [V] ou Falso [F]): ");
+            String resp = sc.nextLine().toUpperCase();
+
+            boolean respB = resp.equals("V") ? true : resp.equals("F") ? false : false;
+
+            if (respB == RESPOSTAS_VF[indp - PERGUNTAS_ME.length]) {
+                System.out.println("Resposta correta! Você ganhou 10 pontos");
+                pont += 10;
+            } else {
+                System.out.println("Resposta incorreta. A resposta certa é: " + (RESPOSTAS_VF[indp - PERGUNTAS_ME.length] ? "Verdadeiro" : "Falso"));
             }
         }
 
