9eabdab62270fbd14a0312e3b3d72970563e820c
Manerich <e.manerich@hotmail.com>
2024-05-23 16:44:45 -0300

    Correção de erros no código

--- src/JogoAdivinhacao.java
+++ src/JogoAdivinhacao.java
@@ -11,25 +11,21 @@ public class JogoAdivinhacao {
     public static void main(String[] args) {
         boolean jogarNovamente = true;
         final int numMin = 1;
-        int tent = 1;
-        boolean win = false;
-        boolean dica = false;
 
         System.out.println("\nBem-Vindo ao Jogo da Adivinhação!");
 
         while (jogarNovamente) {
+            boolean dica = false;
+            boolean win = false;
+            int tent = 1;
+
             choseDificult();
+            int numS = rd.nextInt(numMax) + numMin;
 
-            // Gera de acordo com a escolha de dificuldade, o número secreto que terá de ser descoberto
-            int nums = rd.nextInt(numMax) + numMin;
-
-            // Se ainda não foi acertado o número secreto e as tentativas ainda estão abaixo do máximo de tentativas o jogo continua
             while (!win && tent <= tentmax) {
-                // Se foram feitas 4 tentativas e a dica ainda não foi dada, será informada um dica
                 if (tent == 5 && !dica) {
                     dica = true;
-                    // Verifica se o número secreto é par ou ímpar e dá a dica de acordo com o cálculo
-                    if (nums % 2 == 0) {
+                    if (numS % 2 == 0) {
                         System.out.println("\nDica: O Número Secreto é par");
                     } else {
                         System.out.println("\nDica: O Número Secreto é impar");
@@ -37,28 +33,24 @@ public class JogoAdivinhacao {
                 }
 
                 System.out.print("Tentativa " + tent + " de " + tentmax + ". Digite um Número: ");
-
                 int palp = ler.nextInt();
                 tent++;
 
-                // Verifica se o usuário acertou o número secreto
-                if (palp == nums) {
+                if (palp == numS) {
                     win = true;
-                } else if (palp < nums) { // Se o usuário errou, vai ser informado se o palpite dele é menor ou maior que o número secreto
+                } else if (palp < numS) {
                     System.out.println("O Número Secreto é maior");
                 } else {
                     System.out.println("O Número Secreto é menor");
                 }
             }
 
-            // Verifica se o usuário acertou ou excedeu o número de tentativas e informa se ouve vitória ou derrota
             if (win) {
-                System.out.println("##################################\nParabéns! Você acertou o Número Secreto " + nums + " com " + tent + " tentativas");
+                System.out.println("##################################\nParabéns! Você acertou o Número Secreto " + numS + " com " + tent + " tentativas");
             } else {
-                System.out.println("##################################\nVocê exedeu o número de tentativas. O Número Secreto era " + nums);
+                System.out.println("##################################\nVocê exedeu o número de tentativas. O Número Secreto era " + numS);
             }
 
-            // Pergunta se o usuário quer jogar novamente
             System.out.print("Deseja Jogar Novamente? (S/N): ");
             char resp = ler.next().charAt(0);
             jogarNovamente = (resp == 's' || resp == 'S');
@@ -78,7 +70,6 @@ public class JogoAdivinhacao {
             System.out.println("3 - Díficil (Números de 1 a 100, com 5 tentativas)");
             System.out.print("\nDigite o Número correspondente à Dificuldade: ");
 
-            // De acordo com a dificuldade escolhida, muda as váriaveis de número máximo e tentativas máximas
             dificul = ler.nextInt();
             switch (dificul) {
                 case 1:
